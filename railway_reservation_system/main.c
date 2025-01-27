2fdf87b92dbac0709a6abad3ea089453bf72846b
Don-tresor05 <dontresor922@gmail.com>
2025-01-27 14:29:06 +0200

    Day1: Set up menu with switch statement

--- railway_reservation_system/main.c
+++ railway_reservation_system/main.c
@@ -3,6 +3,22 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int choice;
+    printf("Choose an option: \n");
+    printf("1. View available trains\n");
+    printf("2. Book tickets\n");
+    printf("3. Cancel tickets\n");
+    printf("4. Search by destination\n\n");
+
+    printf("Enter your choice: ");
+    scanf("%i", &choice);
+
+    switch(choice)
+    {
+    case 1:
+        printf("View available trains feature coming soon!\n");
+        break;
+    }
+
     return 0;
 }
